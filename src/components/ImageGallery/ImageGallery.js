3013b1a550e6a1038fd031b524d71a5b0322b1e2
Korobass87 <korobass1987@yandex.ru>
2022-03-28 09:50:24 +0300

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -7,26 +7,29 @@ import Button from 'components/Button/Button'
 import "./ImageGallery.scss"
 import Loading from 'components/Loading/Loading'
 
-
+const Status = {
+  LOADING: "loading",
+  REJECTED: "rejected",
+  MODAL: "modal"
+}
 
 export default class ImageGallery extends Component {
   state = {
-    hits: null,
-    showModal: false,
-    modalImg: "",
-    loading: false,
+    hits: null,    
+    modalImg: "",    
     error: null,
     page: 1,
     maxPage: 0,
-    showLoadMore: false
+    showLoadMore: false,
+    status: "waiting"
   }
 
 
   showModal = (img) => {
-    this.setState({modalImg: img, showModal: true})
+    this.setState({modalImg: img, status: Status.MODAL})
   }
   closeModal = () => {
-    this.setState({showModal: false})
+    this.setState({status: Status.REJECTED})
   }
   
   async componentDidUpdate(prevProps, prevState) {
@@ -34,12 +37,12 @@ export default class ImageGallery extends Component {
     const nextName = this.props.imgName;
     
     if  (prevName !== nextName) {
-      await this.setState({loading: true, page: 1, hits: null })
+      await this.setState({status: Status.LOADING, page: 1, hits: null })
 
                 API.FetchImg(nextName, this.state.page)
             .then(data=>{(data.total>0) ? this.setState({hits: data.hits, error: null}) : this.setState({error: true, hits: null}); if(data.total>12){this.setState({maxPage: Math.ceil(data.totalHits/12), showLoadMore: true})}})
             .catch(error=>this.setState({error}))
-            .finally(()=>{this.setState({loading: false})})
+            .finally(()=>{this.setState({status: Status.REJECTED})})
           
     }
 
@@ -49,12 +52,12 @@ export default class ImageGallery extends Component {
 
  loadMore = async ()=>{
     
-     await this.setState({loading: true, page: this.state.page + 1, showLoadMore: false})
+     await this.setState({status: Status.LOADING, page: this.state.page + 1, showLoadMore: false})
         this.scroll()
     
         API.FetchImg(this.props.imgName, this.state.page)
         .then(data => this.setState({hits: [...this.state.hits, ...data.hits]}))
-        .finally(()=>{this.setState({loading: false, showLoadMore: true}); this.scroll()})
+        .finally(()=>{this.setState({status: Status.REJECTED, showLoadMore: true}); this.scroll()})
         
  }
 
@@ -70,8 +73,10 @@ export default class ImageGallery extends Component {
   
   
   render() {
+
+    
     return (
-        <body>
+        <section>
 
         {this.state.error && <div className='wrap'><h2>Картинки с именем <span className='wrapper'>{this.props.imgName}</span> не найдено</h2></div>}
 
@@ -81,13 +86,13 @@ export default class ImageGallery extends Component {
         
         </ul> </>}
         
-        {this.state.loading && <Loading/>}
+        {this.state.status === "loading" && <Loading/>}
 
        { this.state.maxPage !== this.state.page && this.state.hits && this.state.showLoadMore && <Button loadMore={this.loadMore}/> }
        
-        { this.state.showModal && <Modal URL={this.state.modalImg} closeModal={this.closeModal}  />}
+        { this.state.status === "modal" && <Modal  URL={this.state.modalImg} closeModal={this.closeModal}  />}
        
-        </body>
+        </section>
   )
   }
 }
